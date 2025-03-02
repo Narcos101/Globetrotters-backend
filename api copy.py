33ee9d77694031d643579af3bc8d1cc8ff809cf4
Narcos101 <vivekjaiswar899@gmail.com>
2025-03-02 08:51:34 +0530

    migrated to sqlite db

--- api copy.py
+++ api copy.py
@@ -1,25 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask,request,jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
 from io import BytesIO
 from flask_bcrypt import Bcrypt
 from PIL import Image, ImageDraw, ImageFont
-from datetime import timedelta, datetime
+from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import JSON
+from datetime import datetime
+
 from sqlalchemy.orm.attributes import flag_modified
 from flask_cors import CORS
 
 import uuid
-import json
-import os
-import random
+import time,json,os,random,bcrypt
 
 app = Flask(__name__)
 CORS(app)
 
 TOKEN_ETIME = 6 * 60
 
-# SQLite Database Configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///game_db.sqlite"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:root@localhost/game_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = "3ed0b782b00d803b16b17bdcb312a943ba7a4d5d5a896751fc933321c2e0ce31"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=TOKEN_ETIME)
@@ -28,22 +29,17 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
+
 SCORE_TO_BE_ADDED = 10
 
 class GameSession(db.Model):
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    rounds_data = db.Column(db.Text, nullable=False, default='{}')  # Store JSON as text
-    start_time = db.Column(db.DateTime, default=datetime.utcnow)
+    rounds_data = db.Column(JSON, nullable=False, default={})
+    start_time = db.Column(db.DateTime, default=datetime.now())
     end_time = db.Column(db.DateTime, nullable=True)
     correct_questions = db.Column(db.Integer, nullable=False, default=0)
-    incorrect_questions = db.Column(db.Integer, nullable=False, default=0)
-
-    def get_rounds_data(self):
-        return json.loads(self.rounds_data) if self.rounds_data else {}
-    
-    def set_rounds_data(self, data):
-        self.rounds_data = json.dumps(data)
+    incorrect_questions = db.Column(db.Integer, nullable=False,default=0)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -52,6 +48,7 @@ class User(db.Model):
 
 with app.app_context():
     db.create_all()
+
 
 def load_json():
     if not os.path.exists('countries.json'):
@@ -69,16 +66,20 @@ def load_json():
 @jwt_required()
 def start_session():
     user_id = get_jwt_identity()
+    
     session = GameSession(user_id=user_id)
     db.session.add(session)
     db.session.commit()
+
     return jsonify({"message": "Game session started", "session_id": session.id}), 201
+
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
     username = data["username"]
     password = data["password"]
+
     user = User.query.filter_by(username=username).first()
     if user and bcrypt.check_password_hash(user.password, password):
         access_token = create_access_token(identity=user.id)
@@ -93,9 +94,11 @@ def register():
     data = request.json
     username = data["username"]
     password = bcrypt.generate_password_hash(data["password"]).decode("utf-8")
+
     existing_user = User.query.filter_by(username=username).first()
     if existing_user:
         return jsonify({"error": "Username already exists"}), 400
+
     new_user = User(username=username, password=password)
     db.session.add(new_user)
     db.session.commit()
@@ -104,56 +107,60 @@ def register():
 def get_available_countries(used_questions):
     countries, error = load_json()
     while True:
-        random_idx = random.randint(0, len(countries)-1)
+        random_idx = random.randint(0,len(countries)-1)
         if random_idx not in used_questions:
             break
+
     random_country = countries[random_idx]
-    random_options = random.sample(countries, 6)
+    random_options = random.sample(countries, 6)  # Get 6 to have a backup
+
+    # Remove the selected country's name if it's in the options
     random_options = [d['name'] for d in random_options if d['name'] != random_country['name']][:5]
+
     return random_country, random_idx, random_options
 
-@app.route('/api/get_round', methods=["GET", "POST"])
+
+@app.route('/api/get_round', methods=["GET","POST"])
 @jwt_required()
 def get_round():
-    session_id = request.args.get("session_id")
+    # Randomly select a answer destination and a list of hint destinations 
+    # confirm on this whether to throw an error for every endpoint or not    
+    session_id = request.args.get("session_id")      # Get session_id from query params
+
     if not session_id:
         return jsonify({"error": "session_id is required"}), 400
+    
     session = GameSession.query.filter_by(id=session_id, end_time=None).first()
     if not session:
         return jsonify({"error": "No active game session found for this session_id"}), 404
-    rounds_data = session.get_rounds_data()
+    
+    rounds_data = session.rounds_data
+
     used_questions = set(map(int, rounds_data.keys()))
+
     random_country, random_country_idx, random_options = get_available_countries(used_questions)
+
     clues = random_country['clues']
-    options = [random_country['name']] + random_options
+    options = []
+    options.append(random_country['name'])
+    options.extend(random_options)
     random.shuffle(options)
-    new_round = {"isCorrect": False, "selectedOption": -1, "correctOption": random_country['name'],
+
+    new_round = {"isCorrect": False,"selectedOption": -1,"correctOption": random_country['name'],
                 "additional_details": random_country['funFacts']}
+
     rounds_data[random_country_idx] = new_round
-    session.set_rounds_data(rounds_data)
+    session.rounds_data = rounds_data
+    flag_modified(session, "rounds_data")
+    # Update session with new rounds_data
     db.session.commit()
-    return {'uuid': random_country_idx, "roundno": len(rounds_data), 'answer_destination': clues, 'hint_destinations': options}
-
-@app.route('/api/get_user_info', methods=["GET"])
-@jwt_required()
-def get_user_details():
-    user_data = {}
-    user_id = get_jwt_identity()
-    user = User.query.filter_by(id=user_id).first()
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-    sessions = GameSession.query.filter_by(user_id=user_id).all()
-    user_data['username'] = user.username
-    if sessions:
-        total_correct_cnt = sum(session.correct_questions for session in sessions)
-        user_data['total_games_played'] = len(sessions)
-        user_data['total_correct_questions'] = total_correct_cnt
-    return jsonify(user_data), 200
-
+    return {'uuid':random_country_idx,"roundno":len(rounds_data), 'answer_destination': clues, 'hint_destinations': options}
+    
 @app.route('/api/set_round', methods=["POST"])
 @jwt_required()
 def set_round():
     data = request.get_json()
+
     session_id = data.get("session_id")
     uuid = str(data.get("uuid"))
     selected_option = str(data.get("selected_option"))
@@ -165,29 +172,84 @@ def set_round():
     if not session:
         return jsonify({"error": "No active game session found for this session_id"}), 404
 
-    rounds_data = session.get_rounds_data()  # Properly load JSON from text column
+    rounds_data = session.rounds_data
 
     if uuid not in rounds_data:
         return jsonify({"error": "Invalid uuid"}), 400
 
-    is_correct = selected_option == rounds_data[uuid]["correctOption"]
+    is_correct = False
 
-    if is_correct:
-        session.correct_questions += 1
+    # Determine if the selected option is correct
+    if selected_option == rounds_data[uuid]["correctOption"]:
+        is_correct = True
+        session.correct_questions = int(session.correct_questions) + 1
     else:
-        session.incorrect_questions += 1
+        session.incorrect_questions = int(session.incorrect_questions) + 1
 
+    # Update round data
     rounds_data[uuid]["selectedOption"] = selected_option
     rounds_data[uuid]["isCorrect"] = is_correct
 
-    session.set_rounds_data(rounds_data)  # Convert dict to JSON and store
+    session.rounds_data = rounds_data
+
+    session.rounds_data = rounds_data
+
+    flag_modified(session, "rounds_data")
     db.session.commit()
 
     return jsonify({
         "message": "Round updated successfully",
         "updated_round": rounds_data[uuid],
-        "isCorrect": is_correct,
-        "additional_details": rounds_data[uuid].get('additional_details', ''),
-        "correct_questions": session.correct_questions,
-        "incorrect_questions": session.incorrect_questions
-    }), 200
+        "isCorrect": is_correct,  # Include isCorrect in response
+        "additional_details": rounds_data[uuid]['additional_details'],
+        "correct_questions" : session.correct_questions,
+        "incorrect_questions" : session.incorrect_questions
+    })
+
+@app.route('/api/get_user_info', methods=["GET"])
+@jwt_required()
+def get_user_details():
+
+    user_data = {}
+    user_id = get_jwt_identity()
+    user = User.query.filter_by(id=user_id).first()
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    sessions = GameSession.query.filter_by(user_id=user_id).all()
+    user_data['username'] = user.username
+
+    if sessions:
+        total_correct_cnt = sum([session.correct_questions for session in sessions])
+
+        user_data['total_games_played'] = len(sessions)
+        user_data['total_correct_questions'] = total_correct_cnt
+
+    return jsonify(user_data), 200
+
+# @app.route('/invite/<int:user_id>', methods=['GET'])
+# def generate_invite(user_id):
+#     cursor.execute("SELECT username, score FROM users WHERE id=%s", (user_id,))
+#     user = cursor.fetchone()
+    
+#     if not user:
+#         return jsonify({"error": "User not found"}), 404
+
+#     username, score = user
+
+#     # Create an image
+#     img = Image.new('RGB', (400, 200), color='white')
+#     draw = ImageDraw.Draw(img)
+#     font = ImageFont.load_default()
+    
+#     draw.text((20, 50), f"{username}'s Score: {score}", fill='black', font=font)
+#     draw.text((20, 100), "Join me in this game!", fill='black', font=font)
+
+#     buffered = BytesIO()
+#     img.save(buffered, format="PNG")
+#     img_base64 = base64.b64encode(buffered.getvalue()).decode()
+
+#     return jsonify({"image": f"data:image/png;base64,{img_base64}", "invite_link": f"http://localhost:3000/join/{user_id}"})
+
+    
